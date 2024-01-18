d12f2381bfe18cf71e68d7bbe1899e0e7859e2d5
avigyat <avigyat.avi@outlook.com>
2024-01-18 10:05:14 +0530

    adding assets

---
+++ src/components/Navbar.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+import { FaBars,FaTimes } from "react-icons/fa";
+
+const Navbar = () => {
+  return (
+    <div className='flex items-center justify-between w-full h-20 text-white fixed bg-slate-950 '>
+        <h2>Avigyat</h2>
+    </div>
+  )
+}
+
+export default Navbar
